9fff98acab0108458dd0c33ef48863d97af255c7
Kathia Medina Rodriguez <kmedinar1@upao.edu.pe>
2024-06-20 15:53:57 -0500

    featureÃ: comentar evento

---
+++ src/app/coment/coment.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coment',
+  templateUrl: './coment.component.html',
+  styleUrl: './coment.component.css'
+})
+export class ComentComponent {
+  description: string = '';
+
+  coment: Comment = {
+    description: this.description,
+    created_at: new Date(),
+    updated_at: new Date(),
+    user_id: 1,
+    event_id: 1
+  };
+
+  saveComment() {
+    this.coment.description = this.description;
+    fetch('http://localhost:8091/api/v1/coments', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.coment)
+    }).then(response => {
+      console.log(response.body);
+    })
+  }
+}
+
+export interface Comment {
+  description: string;
+  created_at: Date;
+  updated_at: Date;
+  user_id: number;
+  event_id: number;
+}
