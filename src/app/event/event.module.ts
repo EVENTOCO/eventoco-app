19e4d9197c9783f21a5c6025ff35e785364eb35c
Brayan Castillo <bragyannn177@gmail.com>
2024-06-19 22:32:35 -0500

    Implementacion de la funcionalidad de borrar evento.

--- src/app/event/event.module.ts
+++ src/app/event/event.module.ts
@@ -3,15 +3,25 @@ import { CommonModule } from '@angular/common';
 
 import { EventRoutingModule } from './event-routing.module';
 import { EventComponent } from './event.component';
-
+import { CreateEventComponent } from './components/create-event/create-event.component';
+import { SeeTutorialComponent } from './components/see-tutorial/see-tutorial.component';
+import { DeleteEventComponent } from './components/delete-event/delete-event.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from './../material/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    EventComponent
+    EventComponent,
+    DeleteEventComponent
   ],
   imports: [
     CommonModule,
-    EventRoutingModule
+    EventRoutingModule,
+    FormsModule,
+     MaterialModule,
+     ReactiveFormsModule,
+     HttpClientModule
   ]
 })
 export class EventModule { }
