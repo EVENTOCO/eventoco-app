4140884aa08dfcd162a126ae61c4de9352b156cc
Yerson0811 <yerson0811@gmail.com>
2024-07-02 22:27:20 -0500

    Despliegue de API REST

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,4 +1,5 @@
 export const enviroment = {
   production:false,
-  baseUrl:'http://localhost:8080/api/v1'
+  //baseUrl:'http://localhost:8080/api/v1'
+  baseUrl:'https://eventoco-api.onrender.com/api/v1'
 }
