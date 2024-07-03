fac79644f22be66ef077bdaabfed752b57fddf5b
Yerson0811 <yerson0811@gmail.com>
2024-07-03 15:56:13 -0500

    Despliegue de API REST

--- src/app/user/components/register-user/register-user.component.ts
+++ src/app/user/components/register-user/register-user.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-register-user',
@@ -11,7 +12,7 @@ export class RegisterUserComponent {
   userForm: FormGroup;
   isFormVisible = false;
 
-  constructor(private fb: FormBuilder, private userService: UserService) {
+  constructor(private fb: FormBuilder, private userService: UserService, private router: Router) {
     this.userForm = this.fb.group({
       username: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
       password: ['', Validators.required],
@@ -32,7 +33,8 @@ export class RegisterUserComponent {
         next: () => {
           console.log('Registro completado');
           this.isFormVisible = false;
-          window.location.reload(); // Recargar la página actual
+          // window.location.reload(); // Recargar la página actual
+          this.router.navigate(['']);
         },
         error: (error: any) => {
           console.error('Error al registrar usuario', error);
