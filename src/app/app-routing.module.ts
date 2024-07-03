f1660d0b62c3f13c79e43d051064685efccf8aa1
Kathia Medina <kmedinar1@upao.edu.pe>
2024-07-03 11:59:36 -0500

    rediridir a los componentes de login, register y cerrar sesion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent},
+  { path: '', loadChildren: () => import('./landingpage/landingpage.module').then(m => m.LandingpageModule) },
   { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
   { path: 'event', loadChildren: () => import('./event/event.module').then(m => m.EventModule) },
   { path: 'coment', loadChildren: () => import('./coment/coment.module').then(m => m.ComentModule) },
-  { path: 'wellcome', loadChildren: () => import('./landingpage/landingpage.module').then(m => m.LandingpageModule) },
 ];
 
 @NgModule({
--- src/app/landingpage/navbar/navbar.component.ts
+++ src/app/landingpage/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import { Router } from '@angular/router';
+import {UserResponse} from "../../user/model/user-response.model";
+import {UserService} from "../../user/services/user.service";
 
 @Component({
   selector: 'app-landing-navbar',
@@ -6,5 +9,44 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  @Input() user!: UserResponse;
+
+  constructor(private router: Router, private userService: UserService) { }
+
+  toLogin(){
+    this.router.navigate(['login'])
+  }
+
+  toRegister() {
+    this.router.navigate(['register'])
+  }
+
+  logout(): void {
+    this.loadUser()
+    const confirmation = confirm(`¿Seguro de que quieres cerrar la sesión de ${this.user.username}?`);
+    if (confirmation) {
+      localStorage.removeItem('userId');
+      window.location.reload();
+    }
+  }
+
+  loadUser(): void {
+    const userId = localStorage.getItem('userId');
+    if (userId) {
+      this.userService.getUserById(+userId).subscribe({
+        next: (user: UserResponse) => {
+          this.user = user;
+          console.log('Los datos del usuario han sido cargados correctamente:', this.user);
+        },
+        error: (error: any) => {
+          console.error('La sesión ha sido cerrada', error);
+        }
+      });
+    }
+  }
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('userId');
+  }
 
 }
